bd8908fd62b0a6d0738a70c98fc98a8d046f42a2
TemporallyName <aurrutiab@usm.cl>
2025-10-31 18:57:47 -0300

    Delete Eco_Tracker directory

--- Eco_Tracker/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EcoTrackerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Eco_Tracker'
-#Para catalogar todo el contenido como una app
--- Eco_Tracker/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, "ecotracker/index.html")
-#Renderizar los hmtl, es decir que se vea la página xd 
