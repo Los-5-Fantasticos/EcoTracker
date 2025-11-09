68b77acb3a35f9876055c6819edda9ff4f62ed74
ayelencr <ayelendamaris.caceres@gmail.com>
2025-11-09 20:10:59 -0300

    Update urls.py

--- Eco_Tracker/urls.py
+++ Eco_Tracker/urls.py
@@ -6,3 +6,15 @@ urlpatterns = [
     path('', views.index, name="index"),
 ]
 #con esto existe el URL, es decir la página
+    
+    path('registro/', views.crear_usuario, name='registro'), 
+    
+    # RUTA DE INICIO DE SESIÓN
+    path('login/', views.iniciar_sesion, name='login'),
+    
+    # (Otras rutas...)
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    
+    # RUTA DE DESTINO DESPUÉS DEL LOGIN
+    path('perfil/', views.ver_perfil, name='perfil'),
+]
